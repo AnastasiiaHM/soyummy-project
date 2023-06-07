ed00c9b59a22dbb25c59487740c99d51fdaaa61e
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-06-07 10:30:58 +0200

    +

--- src/components/App/App.styled.jsx
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-
-export const MainContainer = styled.div`
-  /* min-height: 100vh; */
-`;
--- src/constants/colors.js
+++
@@ -1,7 +0,0 @@
-export const colors = Object.freeze({
-  color1: '#fc842d',
-  color2: '#9b9faa',
-  color3: '#e0e0e0',
-  color4: '#212121',
-  color5: '#264061',
-});
