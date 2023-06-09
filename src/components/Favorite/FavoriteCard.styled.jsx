54b24ce56c739bbb2fe99bc95feef2f85e2cc083
yanachepyk <yanapgp@gmail.com>
2023-06-09 13:02:51 +0300

    add loader and fixed styles

--- src/components/Favorite/FavoriteCard.styled.jsx
+++ src/components/Favorite/FavoriteCard.styled.jsx
@@ -1,21 +1,23 @@
 import styled from '@emotion/styled';
 import { BiTrash } from 'react-icons/bi';
+import { mediaSizes } from '../constants/media';
+import { colors } from 'components/colors';
 
 export const ContainerCard = styled.div`
-  background: #ffffff;
+  background: ${colors.whiteColor};
   border-radius: 8px;
   display: flex;
   gap: 14px;
   padding: 14px 9px;
   width: 100%;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     display: flex;
     gap: 24px;
     padding: 28px 24px;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     display: flex;
     gap: 40px;
     padding: 40px;
@@ -28,14 +30,14 @@ export const ImageContainer = styled.div`
   width: 124px;
   height: 124px;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     min-width: 228px;
     min-height: 232px;
     height: 232px;
     width: 228px;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     min-width: 318px;
     min-height: 324px;
     width: 318px;
@@ -58,20 +60,19 @@ export const CardHeader = styled.div`
   & > p {
     font-size: 8px;
     line-height: 1.25;
-    color: #23262a;
-
+    color: ${colors.blackFont};
     display: -webkit-box;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
     overflow: hidden;
     text-overflow: ellipsis;
 
-    @media screen and (min-width: 768px) {
+    @media screen and (min-width: ${mediaSizes.tablet}) {
       font-size: 14px;
       line-height: 1.3;
     }
 
-    @media screen and (min-width: 1440px) {
+    @media screen and (min-width: ${mediaSizes.desktop}) {
       font-size: 18px;
       line-height: 1.33;
       -webkit-line-clamp: 8;
@@ -83,17 +84,18 @@ export const DeleteBtn = styled.button`
   width: 24px;
   height: 24px;
   top: 14px;
-  background: #ebf3d4;
+  background: ${colors.imageBC};
   border-radius: 4px;
   border: 0;
   cursor: pointer;
-  @media screen and (min-width: 768px) {
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     width: 38px;
     height: 38px;
     top: 28px;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     width: 44px;
     height: 44px;
     top: 40px;
@@ -103,13 +105,14 @@ export const DeleteBtn = styled.button`
 export const DeleteIcon = styled(BiTrash)`
   width: 14px;
   height: 14px;
-  color: #2a2222;
-  @media screen and (min-width: 768px) {
+  color: ${colors.darkButton};
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     width: 22px;
     height: 22px;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     width: 24px;
     height: 24px;
   }
@@ -119,20 +122,15 @@ export const CardFooter = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: end;
-  @media screen and (min-width: 768px) {
-  }
-
-  @media screen and (min-width: 1440px) {
-  }
 
   & > p {
     font-weight: 500;
     font-size: 10px;
     line-height: 1.4;
     letter-spacing: -0.24px;
-    color: #3e4462;
+    color: ${colors.recipeCardText};
 
-    @media screen and (min-width: 768px) {
+    @media screen and (min-width: ${mediaSizes.tablet}) {
       font-size: 14px;
     }
   }
@@ -144,47 +142,45 @@ export const CardTitle = styled.h3`
   font-size: 14px;
   line-height: 1;
   letter-spacing: -0.24px;
-  color: #3e4462;
-  @media screen and (min-width: 768px) {
+  color: ${colors.recipeCardText};
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     margin-bottom: 18px;
     font-size: 24px;
     line-height: 1;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     margin-bottom: 30px;
   }
 `;
 
 export const PrimaryBtn = styled.button`
-  background-color: #22252a;
+  background-color: ${colors.darkButton};
   border-radius: 24px 44px;
   border: 0;
   width: 87px;
   height: 27px;
-
   font-size: 10px;
-  line-height: 1, 5;
-  color: #fafafa;
+  line-height: 1.5;
+  color: ${colors.textGreenBtn};
   cursor: pointer;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     width: 138px;
     height: 45px;
     font-size: 14px;
-    line-height: 1.5;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     width: 160px;
     height: 54px;
     font-size: 16px;
-    line-height: 1.5;
   }
 
   &:hover,
   &:focus {
-    background-color: #8baa36;
+    background-color: ${colors.greenButton};
   }
 `;
 
@@ -192,11 +188,11 @@ export const CardBody = styled.div`
   display: flex;
   flex-grow: 1;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${mediaSizes.tablet}) {
     gap: 76px;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${mediaSizes.desktop}) {
     gap: 79px;
   }
 `;
