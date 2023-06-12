3ada0c7d111f0b48a39b27c48ac6e6758b5f5361
mafioz359 <mishazlupko@gmail.com>
2023-06-12 17:00:54 +0300

    Register and login

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,20 +1,51 @@
 import { createSlice } from '@reduxjs/toolkit';
+// import { register, logIn } from './operations';
+import { register, LogIn } from './operations';
 
 const initialState = {
-  user: { name: null, email: null },
+  message: null,
   token: null,
+  user: {
+    name: null,
+    email: null,
+    password: null,
+    _id: null,
+    avatarURL: null,
+    shoppingList: [],
+  },
   isLoggedIn: false,
-
-  isLoading: false,
+  authError: null,
   error: null,
 };
 
-const authSlice = createSlice({
-  name: 'auth',
+const userSlice = createSlice({
+  name: 'user',
   initialState,
-//   extraReducers: builder => {
-//     builder;
-//     //   .addCase(register.fulfilled, registerFulfilledReducer)
-//   },
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        const { name, email, _id } = action.payload.user;
+        state.user._id = _id;
+        state.user.name = name;
+        state.user.email = email;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.authError = null;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.authError = action.payload;
+      })
+      .addCase(LogIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.authError = null;
+      })
+      .addCase(LogIn.rejected, (state, action) => {
+        state.authError = action.payload;
+      })
+      .addCase(LogIn.pending, (state, action) => {});
+  },
 });
-export const authReducer = authSlice.reducer;
+
+export const authReducer = userSlice.reducer;
