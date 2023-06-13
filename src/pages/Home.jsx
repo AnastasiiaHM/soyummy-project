36543d666e077a2c900d0bea124be19f54f10b3a
JuliaMaidan <115953663+JuliaMaidan@users.noreply.github.com>
2023-06-13 19:10:36 +0300

    створила секцію main та стилізувала її

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,17 @@
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import {
-  Container,
-  Title,
-  TitleText,
-  Text,
-  TipBox,
-  BoxText,
-  DecorBoxText,
-  BoxLinkText,
-  DecorArrowIcon,
-  Section,
-} from '../components/Styled/MainPageHome.styled';
+import { Section } from '../components/Styled/MainPageHome.styled';
 import RecipesGallery from '../components/RecipesGallery/RecipesGallery';
-import { SearchBar } from '../components/SearchBar/SearchBar';
+import { SearchForm } from '../components/SearchForm/SearchForm';
 import {
   fetchRecipesByCategory,
   fetchCategory,
 } from '../redux/recipes/operations';
+import { BsArrowRight } from 'react-icons/bs';
 
 export default function MainPageHome() {
+  const location = useLocation();
   const dispatch = useDispatch();
   const { recipes, filter } = useSelector(state => state.categories);
 
@@ -30,26 +22,32 @@ export default function MainPageHome() {
 
   return (
     <Section>
-      <Container>
-        <Title>
-          <TitleText>So</TitleText>Yummy
-        </Title>
-        <Text>
-          "What to cook?" is not only a recipe app, it is, in fact,{'\n'}
-          your cookbook. You can add your own recipes to{'\n'}
-          save them for the future.
-          <SearchBar />
-        </Text>
-        <TipBox>
-          <BoxText>
-            <DecorBoxText>Delicious and healthy</DecorBoxText> way to enjoy a
-            variety of fresh ingredients in one satisfying meal
-          </BoxText>
-          <BoxLinkText>See recipes</BoxLinkText>
-          <DecorArrowIcon />
-        </TipBox>
-        <RecipesGallery recipes={recipes} />
-      </Container>
+      <div className="wrapper">
+        <div className="main">
+          <h1>
+            <span>So</span>Yummy
+          </h1>
+          <p>
+            "What to cook?" is not only a recipe app, it is, in fact, your
+            cookbook. You can add your own recipes to save them for the future.
+          </p>
+          <SearchForm />
+        </div>
+        <div className="tips">
+          <p>
+            <span>Delicious and healthy </span>
+            way to enjoy a variety of fresh ingredients in one satisfying meal
+          </p>
+          <div className="link">
+            <Link to={`/categories`} state={{ from: location }}>
+              See recipes
+            </Link>
+            <BsArrowRight />
+          </div>
+        </div>
+      </div>
+
+      <RecipesGallery recipes={recipes} />
     </Section>
   );
 }
