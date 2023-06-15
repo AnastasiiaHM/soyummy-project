91138b09a0f895c581a3fca9b04bf018ee1fbb9b
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-06-15 18:18:54 +0200

    clean

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectLoggedIn } from 'redux/auth/selectors';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(state => state.user.token);
--- src/pages/AddRecipes/AddRecipes.jsx
+++ src/pages/AddRecipes/AddRecipes.jsx
@@ -31,7 +31,7 @@ const AddRecipes = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
