9c1e2f7709b6b64be517284712a5d044ae139ed3
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-06-15 11:27:27 +0300

    123

--- src/components/RecipesGallery/RecipeCard/RecipeCard.jsx
+++ src/components/RecipesGallery/RecipeCard/RecipeCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { RecipeCardStyled } from './RecipeCardStyled';
+import { RecipeCardStyled, StyledLink } from './RecipeCardStyled';
 
-export const RecipeCard = ({ recipe }) => {
+export const RecipeCard = ({ recipe, location }) => {
   return (
     <RecipeCardStyled>
-      <a href="*">
+      <StyledLink to={`/recipe/${recipe._id}`} state={{ from: location }}>
         <img className="image" src={recipe.preview} alt="dish-card" />
         <p className="name">{recipe.title}</p>
-      </a>
+      </StyledLink>
     </RecipeCardStyled>
   );
 };
