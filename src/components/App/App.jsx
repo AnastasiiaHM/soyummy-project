4fec98f6d6b0d11265c88b4687cba75dcda25698
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-06-15 15:36:50 +0200

    private and restricted rout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,8 @@ import { MainContainer } from './App.styled';
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout/Layout';
-// import { RestrictedRoute } from 'components/RestrictedRoute';
-// import { PrivateRoute } from 'components/PrivateRoute';
+import { RestrictedRoute } from 'components/RestrictedRoute';
+import { PrivateRoute } from 'components/PrivateRoute';
 
 const WelcomePage = lazy(() => import('pages/WelcomePage'));
 const RegisterPage = lazy(() => import('pages/Register'));
@@ -25,94 +25,103 @@ const CategoriesRecipes = lazy(() =>
 const App = () => {
   return (
     <>
-      <MainContainer>
+      <div>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route
               path="/"
               index
-              element={<WelcomePage />}
-              // element={
-              //   <RestrictedRoute component={WelcomePage} redirectTo="/main" />
-              // }
+              // element={<WelcomePage />}
+              element={
+                <RestrictedRoute
+                  component={<WelcomePage />}
+                  redirectTo="/main"
+                />
+              }
             />
             <Route
               path="/signin"
-              element={<LoginPage />}
-              // element={
-              //   <RestrictedRoute
-              //     component={LoginPage}
-              //     redirectTo="/main"
-              //     replace={true}
-              //   />
-              // }
+              // element={<LoginPage />}
+              element={
+                <RestrictedRoute
+                  component={<LoginPage />}
+                  redirectTo="/main"
+                  replace={true}
+                />
+              }
             />
             <Route
               path="/register"
-              element={<RegisterPage />}
-              // element={
-              //   <RestrictedRoute
-              //     component={RegisterPage}
-              //     redirectTo="/main"
-              //     replace={true}
-              //   />
-              // }
+              // element={<RegisterPage />}
+              element={
+                <RestrictedRoute
+                  component={<RegisterPage />}
+                  redirectTo="/main"
+                  replace={true}
+                />
+              }
             />
             <Route
               path="/main"
-              element={<MainPage />}
-              // element={<PrivateRoute component={MainPage} redirectTo="/" />}
+              // element={<MainPage />}
+              element={<PrivateRoute component={<MainPage />} redirectTo="/" />}
             />
 
             <Route
               path="/categories"
-              element={<CategoriesPage />}
-              // element={
-              //   <PrivateRoute component={CategoriesPage} redirectTo="/" />
-              // }
+              // element={<CategoriesPage />}
+              element={
+                <PrivateRoute component={<CategoriesPage />} redirectTo="/" />
+              }
             >
               <Route path=":categoryName" element={<CategoriesRecipes />} />
             </Route>
             <Route
               path="/add"
-              element={<AddRecipePage />}
-              // element={
-              //   <PrivateRoute component={AddRecipePage} redirectTo="/" />
-              // }
+              // element={<AddRecipePage />}
+              element={
+                <PrivateRoute component={<AddRecipePage />} redirectTo="/" />
+              }
             />
             <Route
               path="/favorite"
-              element={<FavoritesPage />}
-              // element={
-              //   <PrivateRoute component={FavoritesPage} redirectTo="/" />
-              // }
+              // element={<FavoritesPage />}
+              element={
+                <PrivateRoute component={<FavoritesPage />} redirectTo="/" />
+              }
             />
             <Route
               path="/recipe/:recipeId"
-              element={<RecipePage />}
-              // element={<PrivateRoute component={RecipePage} redirectTo="/" />}
+              // element={<RecipePage />}
+              element={
+                <PrivateRoute component={<RecipePage />} redirectTo="/" />
+              }
             />
             <Route
               path="/own-recipes"
-              element={<MyRecipesPage />}
-              // element={
-              //   <PrivateRoute component={MyRecipesPage} redirectTo="/" />
-              // }
+              // element={<MyRecipesPage />}
+              element={
+                <PrivateRoute component={<MyRecipesPage />} redirectTo="/" />
+              }
             />
             <Route
               path="/shopping-list"
-              element={<ShoppingPage />}
-              // element={<PrivateRoute component={ShoppingPage} redirectTo="/" />}
+              // element={<ShoppingPage />}
+              element={
+                <PrivateRoute component={<ShoppingPage />} redirectTo="/" />
+              }
             />
             <Route
               path="/search"
-              element={<SearchPage />}
-              // element={<PrivateRoute component={SearchPage} redirectTo="/" />}
+              // element={<SearchPage />}
+              element={
+                <PrivateRoute component={<SearchPage />} redirectTo="/" />
+              }
             />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
-      </MainContainer>
+      </div>
     </>
   );
 };
--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,9 +1,9 @@
-// import { Navigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { selectIsLoggedIn } from 'redux/user/selectors';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectLoggedIn } from 'redux/auth/selectors';
 
-// export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
-//   const isLoggedIn = useSelector(selectIsLoggedIn);
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(state => state.user.token);
 
-//   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-// };
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
