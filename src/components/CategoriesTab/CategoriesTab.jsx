62082135cc4fe61ddd21e8962a2de94405785a41
Andrii Slabkyi <aslabkyi@gmail.com>
2023-06-18 15:51:14 +0300

    delete comments

--- src/components/CategoriesTab/CategoriesTab.jsx
+++ src/components/CategoriesTab/CategoriesTab.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-// import { Link } from 'react-router-dom';
 import Tabs from '@mui/material/Tabs';
 import { StyledTab, StyledBox } from './CategoriesTab.styled';
 import { useDispatch } from 'react-redux';
@@ -9,38 +8,18 @@ import { useNavigate } from 'react-router-dom';
 export default function CategoriesTab(props) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const location = useLocation();
   const { categoriesList } = props;
   const [value, setValue] = useState(0);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
   const onClickCategory = e => {
     dispatch(setFilter(e));
     navigate(`/categories/${e}`);
   };
-  // useEffect(() => {
-  //   setValue(getInitialValue(location.pathname));
-  // }, [location.pathname]);
 
-  // function getInitialValue(pathname) {
-  //   switch (pathname) {
-  //     case '/Beef':
-  //       return 0;
-  //     case '/Breakfast':
-  //       return 1;
-  //     case '/Dessert':
-  //       return 2;
-  //     case '/Goat':
-  //       return 3;
-  //     case '/Lamb':
-  //       return 4;
-  //     case '/Miscellaneous':
-  //       return 5;
-  //     default:
-  //       return null;
-  //   }
-  // }
   return (
     <StyledBox>
       <Tabs
@@ -53,8 +32,6 @@ export default function CategoriesTab(props) {
       >
         {categoriesList.map(category => (
           <StyledTab
-            // component={Link}
-            // to={`/categories/${category.name}`}
             key={category._id}
             label={category.name}
             onClick={() => onClickCategory(category.name)}
