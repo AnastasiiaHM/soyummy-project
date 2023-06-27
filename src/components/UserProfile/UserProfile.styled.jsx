7ba0cc2b03edb691059e9919c09079e3d2618e2d
Anastasiia <schili@outlook.com>
2023-06-27 20:24:08 +0300

    Update UserProfile.styled.jsx

--- src/components/UserProfile/UserProfile.styled.jsx
+++ src/components/UserProfile/UserProfile.styled.jsx
@@ -47,6 +47,9 @@ export const FormModal = styled.form`
 
 export const WrapperInp = styled.div`
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const InputMod = styled.input`
@@ -55,6 +58,7 @@ export const InputMod = styled.input`
   padding-top: 16px;
   padding-left: 51px;
   padding-bottom: 16px;
+  color: #23262a;
   width: 282px;
   height: 48px;
   @media screen and (min-width: ${mediaSizes.tablet}) {
@@ -73,7 +77,6 @@ export const InputMod = styled.input`
     font-weight: 400;
     font-size: 14px;
     line-height: 1.5;
-    color: #23262a;
     letter-spacing: -0.02em;
     @media screen and (min-width: ${mediaSizes.tablet}) {
       font-size: 18px;
@@ -184,18 +187,9 @@ export const InputPen = styled(BiPencil)`
   width: 24px;
   height: 24px;
   position: absolute;
-  top: 12px;
   right: 18px;
   fill: #23262a;
   cursor: pointer;
-
-  @media screen and (min-width: ${mediaSizes.tablet}) {
-    top: 16px;
-  }
-
-  @media screen and (min-width: ${mediaSizes.desktop}) {
-    top: 16px;
-  }
 `;
 
 export const UserIconInp = styled(BiUser)`
@@ -203,16 +197,8 @@ export const UserIconInp = styled(BiUser)`
   height: 24px;
   position: absolute;
   left: 18px;
-  top: 12px;
   fill: #23262a;
 
-  @media screen and (min-width: ${mediaSizes.tablet}) {
-    top: 16px;
-  }
-
-  @media screen and (min-width: ${mediaSizes.desktop}) {
-    top: 16px;
-  }
 `;
 
 export const BackdropModal = styled.div`
