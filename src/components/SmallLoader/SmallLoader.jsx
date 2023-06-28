1bef885643442f95f31f44262cbf15ccce6d837c
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-06-28 21:18:49 +0200

    fixed category

--- src/components/SmallLoader/SmallLoader.jsx
+++ src/components/SmallLoader/SmallLoader.jsx
@@ -7,18 +7,21 @@ import {
   Circle6,
   Circle7,
   DNAAnimation,
+  Wrapper,
 } from './SmallLoader.styled';
 
 export const Loader = () => {
   return (
-    <DNAAnimation>
-      <Circle1>S</Circle1>
-      <Circle2>o</Circle2>
-      <Circle3>Y</Circle3>
-      <Circle4>u</Circle4>
-      <Circle5>m</Circle5>
-      <Circle6>m</Circle6>
-      <Circle7>y</Circle7>
-    </DNAAnimation>
+    <Wrapper>
+      <DNAAnimation>
+        <Circle1>S</Circle1>
+        <Circle2>o</Circle2>
+        <Circle3>Y</Circle3>
+        <Circle4>u</Circle4>
+        <Circle5>m</Circle5>
+        <Circle6>m</Circle6>
+        <Circle7>y</Circle7>
+      </DNAAnimation>
+    </Wrapper>
   );
 };
